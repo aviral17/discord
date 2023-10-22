dbe594fd02effdaffe978d50e0784fc97a777d9c
aviral <avi14320@gmail.com>
2023-10-23 01:25:41 +0530

    bug fixed

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -85,8 +85,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     {...field}
                   /> */}
                   <Input
-                    // disabled={isLoading}
-                    disabled
+                    disabled={isLoading}
+                    // disabled
                     className={`px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 ${
                       isLoading
                         ? "text-zinc-400 dark:text-zinc-500 opacity-50"
--- middleware.ts
+++ middleware.ts
@@ -11,4 +11,4 @@ export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
-// we are using publicRoutes with the link in middleware.ts of nextjs 13 because we want to make the /api/uploadthing route public, meaning that it doesn’t require authentication to access. We are also using a custom matcher array to apply authMiddleware to all routes except those that contain a dot (.), start with _next, or start with /api or /trpc. This way, we can protect your routes with Clerk authentication and customize which routes are public or ignored.
+// we are using publicRoutes with the link in middleware.ts of nextjs 13 because we want to make the /api/uploadthing route public, meaning that it doesn’t require authentication to access. We are also using a custom matcher array to apply authMiddleware to all routes except those that contain a dot (.), start with _next, or start with /api or /trpc. This way, we can protect our routes with Clerk authentication and customize which routes are public or ignored.
