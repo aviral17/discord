c75677cf85f2fad6f8172431c733b8cb76dbc00a
Aviral <36530173+aviral17@users.noreply.github.com>
2025-05-02 23:47:27 +0530

    Update media-room.tsx

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -15,6 +15,7 @@ interface MediaRoomProps {
   audio: boolean;
 }
 
+// update it as per latest changes
 export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
   const { user } = useUser();
   const [token, setToken] = useState("");
