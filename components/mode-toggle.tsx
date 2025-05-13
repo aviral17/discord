405b2398141239a493e3bbdc84aebf41589fae56
Aviral <36530173+aviral17@users.noreply.github.com>
2025-05-13 23:35:26 +0530

    Update mode-toggle.tsx

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+// updated it as per latest changes
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
