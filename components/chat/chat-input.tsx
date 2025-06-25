fca2942807acf8207ed6da5d46dd1752334d8118
Aviral <36530173+aviral17@users.noreply.github.com>
2025-06-25 23:46:23 +0530

    Update chat-input.tsx

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -39,7 +39,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
     },
   });
 
-  // updateddddd file as per latest changes
+  // updateddddddd file as per latest changes
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
