42e4e8637bfba3ec78dfee510eb9c56281241b48
Aviral <36530173+aviral17@users.noreply.github.com>
2025-07-04 23:43:11 +0530

    Update chat-input.tsx

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -9,7 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-// update file as per latest changes
+// updatedd file as per latest changes
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
