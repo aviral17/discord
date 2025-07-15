a5eca60c351691853146273995038ab952461ad9
Aviral <36530173+aviral17@users.noreply.github.com>
2025-07-15 23:46:43 +0530

    Update chat-header.tsx

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -15,7 +15,7 @@ interface ChatHeaderProps {
   imageUrl?: string;
 }
 
-// update file as per latest changes
+// updated file as per latest changes
 export const ChatHeader = ({
   serverId,
   name,
